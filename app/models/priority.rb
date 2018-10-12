75404ab6429f5bc848cced1ae1909f4149d8616e
Rea Tanguilig <reat@magenic.com>
2018-10-12 15:23:43 +0800

    User story 7: Task management - created controller and views for tasks - created new models: priority, status, category

---
+++ app/models/priority.rb
@@ -0,0 +1,3 @@
+class Priority < ApplicationRecord
+  belongs_to :task
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_092051) do
+ActiveRecord::Schema.define(version: 2018_10_12_062519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "task_id"
@@ -33,9 +39,21 @@ ActiveRecord::Schema.define(version: 2018_10_11_092051) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "priorities", force: :cascade do |t|
+    t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
