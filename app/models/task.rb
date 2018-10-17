c223ff424bd8d3df22434754a17abc247d21513d
rolandj111 <rolandejuan@gmail.com>
2018-10-17 16:05:44 +0800

    Fixes for model relationships

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,9 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :file_attachments
-  has_one :category
-  has_one :priority
-  has_one :status
+  belongs_to :category
+  belongs_to :priority
+  belongs_to :status
 
   validates :category, presence: true
   validates :priority, presence: true
