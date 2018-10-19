2ea6def0a379c2f1e488a0db874c00d452eaca88
rolandj111 <rolandejuan@gmail.com>
2018-10-19 14:41:19 +0800

    routing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :tasks
+  resources :tasks do
+    resources :comments
+  end
   resources :projects
   get "dashboard/index"
   root "dashboard#index"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
